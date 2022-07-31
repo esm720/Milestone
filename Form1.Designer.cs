f5e64a4d831fd11d8f438acff987da8caddca1c7
esm720 <esm720@gmail.com>
2022-07-31 16:21:17 -0700

    Milestone 2

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,11 +29,13 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.AddButton = new System.Windows.Forms.Button();
+            this.RemoveButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(240, 182);
+            this.button1.Location = new System.Drawing.Point(151, 107);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(341, 102);
             this.button1.TabIndex = 0;
@@ -41,11 +43,33 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // AddButton
+            // 
+            this.AddButton.Location = new System.Drawing.Point(75, 264);
+            this.AddButton.Name = "AddButton";
+            this.AddButton.Size = new System.Drawing.Size(215, 91);
+            this.AddButton.TabIndex = 1;
+            this.AddButton.Text = "Add Item";
+            this.AddButton.UseVisualStyleBackColor = true;
+            this.AddButton.Click += new System.EventHandler(this.AddButton_Click);
+            // 
+            // RemoveButton
+            // 
+            this.RemoveButton.Location = new System.Drawing.Point(326, 264);
+            this.RemoveButton.Name = "RemoveButton";
+            this.RemoveButton.Size = new System.Drawing.Size(206, 91);
+            this.RemoveButton.TabIndex = 2;
+            this.RemoveButton.Text = "Remove Item";
+            this.RemoveButton.UseVisualStyleBackColor = true;
+            this.RemoveButton.Click += new System.EventHandler(this.RemoveButton_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.RemoveButton);
+            this.Controls.Add(this.AddButton);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -57,5 +81,7 @@
         #endregion
 
         private Button button1;
+        private Button AddButton;
+        private Button RemoveButton;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -2,7 +2,9 @@ namespace Milestone
 {
     public partial class Form1 : Form
     {
-        //ENRIQUE SANCHEZMURILLO CST-150 MILESTONE
+        //ENRIQUE SANCHEZMURILLO CST-150 MILESTONE 2
+
+        //Below we have our inventory class item.
         class inventoryItem
         {
             public string name;
@@ -15,42 +17,121 @@ namespace Milestone
         {
             InitializeComponent();
         }
-        public void PlayStation()
+        //Method that creates item and adjust quantity.
+        public void PlayStation(String inventoryAction, int adjustQuant)
         {
+            
+            //Here we creat our item by using the class item "inventoryItem"
             inventoryItem playStation = new inventoryItem();
-            playStation.name = "Playstation 5";
-            playStation.description = "Video Game Console";
-            playStation.price = 500;
-            playStation.quantity = 3;
-            if (playStation.quantity != 0 && playStation.quantity > 0)
-            {
-                if (playStation.quantity < 5)
+
+                playStation.name = "Playstation 5";
+                playStation.description = "Video Game Console";
+                playStation.price = 500;
+                playStation.quantity = 3;
+                if (playStation.quantity != 0 && playStation.quantity > 0)
                 {
-                    playStation.stock = "Limited Stock: " + playStation.quantity + " left";
+                    if (playStation.quantity < 5)
+                    {
+                        playStation.stock = "Limited Stock: " + playStation.quantity + " left";
+                    }
+                    else
+                    {
+                        playStation.stock = playStation.quantity + " In Stock";
+                    }
                 }
                 else
                 {
-                    playStation.stock = playStation.quantity + " In Stock";
+                    playStation.stock = "Out of Stock";
                 }
+            
+            
+
+            //Show Inventory 
+            if (inventoryAction.Equals("showInventory")){
+                ViewPlaystationInventory(playStation);
             }
-            else
+
+
+            //Add item from quantity, will adjust to permanently change quantity later.
+            if (inventoryAction.Equals("add"))
             {
-                playStation.stock = "Out of Stock";
-            }         
-            ViewPlaystationInventory(playStation);
+                playStation.quantity = playStation.quantity + adjustQuant;
+                //Logic to adjust stock statement
+                if (playStation.quantity != 0 && playStation.quantity > 0)
+                {
+                    if (playStation.quantity < 5)
+                    {
+                        playStation.stock = "Limited Stock: " + playStation.quantity + " left";
+                    }
+                    else
+                    {
+                        playStation.stock = playStation.quantity + " In Stock";
+                    }
+                }
+                else
+                {
+                    playStation.stock = "Out of Stock";
+                }
+                ViewPlaystationInventory(playStation);
+            }
+
+
+            //Remove item from quantity
+            if (inventoryAction.Equals("remove"))
+            {
+                playStation.quantity = playStation.quantity - adjustQuant;
+
+                //adjust stock statement
+                if (playStation.quantity != 0 && playStation.quantity > 0)
+                {
+                    if (playStation.quantity < 5)
+                    {
+                        playStation.stock = "Limited Stock: " + playStation.quantity + " left";
+                    }
+                    else
+                    {
+                        playStation.stock = playStation.quantity + " In Stock";
+                    }
+                }
+                else
+                {
+                    playStation.stock = "Out of Stock";
+                }
+                ViewPlaystationInventory(playStation);
+            }
+
         }
+        //Method to show our inventory
         private void ViewPlaystationInventory(inventoryItem item)
         {
             MessageBox.Show(item.name + "\n" + item.description + "\n" + item.price.ToString("c") + "\n" + item.stock);
         }
+
+        //Below we have our driver methods to test the inventory class item.
+
+        //Event handler for "Show Inventory button named button1"
         private void button1_Click(object sender, EventArgs e)
         {
-            PlayStation();
+            PlayStation("showInventory",0);
         }
-
+        //Handles the Add Item button by invoking the Playstation method to add to the quantity
+        //We will only sent a quantity of 1, that will be determined by the user later.
+        private void AddButton_Click(object sender, EventArgs e)
+        {
+            PlayStation("add", 1);
+            MessageBox.Show("One Item Added"); //later we will reflect the actual amount added.
+        }
+        //Removes item by sending the string "remove", the invoked method determines what do to.
+        private void RemoveButton_Click(object sender, EventArgs e)
+        {
+            PlayStation("remove",1);
+            MessageBox.Show("One Item Removed");
+        }
+        //Here we load our item as soon as the form loads.
         private void Form1_Load(object sender, EventArgs e)
         {
-
+           //To be used at a later time
+            
         }
     }
 }
